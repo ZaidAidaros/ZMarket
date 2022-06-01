425a93dd79d22ec1bf643c3b7136e19a9549524a
Zaid Aidaros <106095323+ZaidAidaros@users.noreply.github.com>
2022-06-01 10:25:38 +0300

    Create dir  widgets & file showtext

---
+++ lib/widgets/show_text.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class ShowText{
+    
+    Widget titleText(String title){
+        return Text();
+    }
+    
+    detialsText(String details){
+        return Text();
+    }
+    
+    descriptionText(String details,int sizetext){
+        return Text();
+    }
+}
